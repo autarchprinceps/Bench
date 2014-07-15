09101ed4057ed2af0e0e678c42d8602593c4678c
autarch princeps <autarch@outlook.com>
2014-07-15 22:21:47 +0200

    More stuff

--- test.go
+++ test.go
@@ -1,5 +1,10 @@
 package main
 
+/**
+ * TODO
+ * metrics
+ */
+
 import (
 	"log"
 	"os"
@@ -11,18 +16,16 @@ import (
 	"sync"
 )
 
-func create(wg sync.WaitGroup, path string, size int) {
-	defer wg.Done()
+func create(path string, size int) {
 	log.Println("Create: " + path)
 	handle, err := os.Create(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	rand.Seed(time.Now().UnixNano())
-	
 	buf := new(bytes.Buffer)
 	for i := 0; i < size; i++ {
-		r := rand.Uint32
+		r := rand.Uint32()
 		binary.Write(buf, binary.LittleEndian, r)
 		handle.Write(buf.Bytes())
 	}
@@ -30,25 +33,43 @@ func create(wg sync.WaitGroup, path string, size int) {
 	log.Println("Created: " + path)
 }
 
-func read(wg sync.WaitGroup, path string, num int) {
+func read(wg sync.WaitGroup, path string, num int, blockSize int) {
 	defer wg.Done()
 	handle, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+	data := make([]byte, blockSize)
 	for i := 0; i < num; i++ {
-		
+		// pos := rand.Int31n(len(handle)) TODO replace len(handle) with file length
+		_, err := handle.ReadAt(data, pos)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
-func write(path string, num int) {
+func write(wg sync.WaitGroup, path string, num int, blockSize int) {
 	defer wg.Done()
+	handle, err := os.OpenFile(path, os.O_wronly, 0777) // TODO os.ModePerm
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make([]byte, blockSize)
+	for i := 0; i < num; i++ {
+		// TODO fill data with rand
+		// TODO pos
+		_, err := handle.WriteAt(data, pos)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func main() {
 	var wg sync.WaitGroup
 	
+	// param parsing
 	size, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		log.Fatal(err)
@@ -69,15 +90,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	blockSize, err := strconv.Atoi(os.Args[7])
+	if err != nil {
+		log.Fatal(err)
+	}
 	path := os.Args[1]
-	create(path, size)
+	
+	// fileops
+	if size > 0 {
+		create(path, size)
+	}
 	wg.Add(readThr)
 	for i := 0; i < readThr; i++ {
-		go read(path, readNum)
+		go read(wg, path, readNum, blockSize)
 	}
 	wg.Add(writeThr)
 	for i := 0; i < writeThr; i++ {
-		go write(path, writeNum)
+		go write(wg, path, writeNum, blockSize)
 	}
 	wg.Wait()
 }
